0fb1f1a4f779522a060e7965167cbeb41562e715
Lucian B <lucian.bratu@stud.ubbcluj.ro>
2024-05-04 14:00:38 +0300

    Implement record voice while app is running functionality

--- lib/components/dialogs.dart
+++ lib/components/dialogs.dart
@@ -1,12 +1,144 @@
 import 'dart:convert';
 import 'dart:io';
 
+import 'package:audioplayers/audioplayers.dart';
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:utter_art/database/database_helper.dart';
 
 import '../api/api.dart';
 
 class Dialogs {
+  static Future<void> showAudioPreviewDialog(PlatformFile file, BuildContext context) async {
+    AudioPlayer audioPlayer = AudioPlayer();
+    return showDialog<void>(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Audio Preview', textAlign: TextAlign.center),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: <Widget>[
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    IconButton(
+                      icon: const Icon(Icons.play_arrow),
+                      onPressed: () async {
+                        await audioPlayer.play(DeviceFileSource(file.path!)); // Assuming file.path contains the local file path
+                      },
+                    ),
+                    IconButton(
+                      icon: const Icon(Icons.pause),
+                      onPressed: () async {
+                        await audioPlayer.pause();
+                      },
+                    ),
+                    IconButton(
+                      icon: const Icon(Icons.stop),
+                      onPressed: () async {
+                        await audioPlayer.stop();
+                      },
+                    )
+                  ],
+                ),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: const Text('Cancel'),
+              onPressed: () async {
+                await audioPlayer.stop();
+                if(!context.mounted) return;
+                Navigator.of(context).pop();
+              },
+            ),
+            TextButton(
+              child: const Text('Obtain Transcription'),
+              onPressed: () async {
+                await audioPlayer.stop();
+                if(!context.mounted) return;
+                String? prediction = await Api.uploadFile(File(file.path!));
+                if(prediction != null){
+                  if(!context.mounted) return;
+                  await Dialogs.showConfirmationDialog(prediction, context);
+                }
+                if(!context.mounted) return;
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  static Future<void> showAudioPreviewDialogForRecording(File file, BuildContext context) async {
+    AudioPlayer audioPlayer = AudioPlayer();
+    return showDialog<void>(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Audio Preview', textAlign: TextAlign.center),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: <Widget>[
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    IconButton(
+                      icon: const Icon(Icons.play_arrow),
+                      onPressed: () async {
+                        await audioPlayer.play(DeviceFileSource(file.path!)); // Assuming file.path contains the local file path
+                      },
+                    ),
+                    IconButton(
+                      icon: const Icon(Icons.pause),
+                      onPressed: () async {
+                        await audioPlayer.pause();
+                      },
+                    ),
+                    IconButton(
+                      icon: const Icon(Icons.stop),
+                      onPressed: () async {
+                        await audioPlayer.stop();
+                      },
+                    )
+                  ],
+                ),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: const Text('Cancel'),
+              onPressed: () async {
+                await audioPlayer.stop();
+                if(!context.mounted) return;
+                Navigator.of(context).pop();
+              },
+            ),
+            TextButton(
+              child: const Text('Obtain Transcription'),
+              onPressed: () async {
+                await audioPlayer.stop();
+                if(!context.mounted) return;
+                String? prediction = await Api.uploadFile(file);
+                if(prediction != null){
+                  if(!context.mounted) return;
+                  await Dialogs.showConfirmationDialog(prediction, context);
+                }
+                if(!context.mounted) return;
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   static Future<void> showConfirmationDialog(String prediction, BuildContext context) async {
     TextEditingController controller = TextEditingController(text: prediction);
     return showDialog<void>(
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
 import 'dart:io';
 
+import 'package:audioplayers/audioplayers.dart';
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:utter_art/components/drawer.dart';
 import 'package:utter_art/components/upload_file_button.dart';
+import 'package:record/record.dart';
 
-import 'api/api.dart';
 import 'components/dialogs.dart';
 import 'database/database_helper.dart';
 
@@ -39,7 +40,47 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   bool _isUploading = false;
+  bool _isRecording = false;
+  bool _hasRecorded = false;
+  String audioPath = '';
+  late Record _audioRecord;
+  late AudioPlayer _audioPlayer;
+  PlayerState _audioPlayerState = PlayerState.stopped;
   final DatabaseHelper _databaseHelper = DatabaseHelper();
+
+  @override
+  void initState() {
+    _audioRecord = Record();
+    super.initState();
+  }
+
+  @override
+  void dispose(){
+    _audioRecord.dispose();
+    super.dispose();
+  }
+
+  Future<void> startRecording() async {
+    try{
+      if(await _audioRecord.hasPermission()){
+        await _audioRecord.start(encoder: AudioEncoder.wav);
+      }
+    }
+    catch(e){
+      print('Error Start Recording: $e');
+    }
+  }
+
+  Future<String?> stopRecording() async {
+    try{
+      String? path = await _audioRecord.stop();
+      return path;
+    }
+    catch(e){
+      print('Error Stop Recording: $e');
+    }
+    return null;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -47,6 +88,67 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.mic),
+            onPressed: () {
+              showDialog(
+                context: context,
+                builder: (BuildContext context) {
+                  return StatefulBuilder(
+                      builder: (BuildContext context, setState){
+                        return AlertDialog(
+                          title: const Text("Record Audio"),
+                          content: Text("Press the Start Record button to record your voice."),
+                          actions: [
+                            TextButton(
+                                onPressed: _isRecording ? null : () async {
+                                  await startRecording();
+                                  setState(() =>
+                                  _isRecording = true
+                                  );
+                                },
+                                child: const Text("Start Recording")
+                            ),
+                            TextButton(
+                                onPressed: !_isRecording ? null : () async {
+                                  String? path = await stopRecording();
+                                  setState(() {
+                                    _isRecording = false;
+                                    _hasRecorded = true;
+                                    if(path != null){
+                                      audioPath = path;
+                                    }
+                                  });
+
+                                },
+                                child: const Text("Stop Recording")
+                            ),
+                            TextButton(
+                                onPressed: !_hasRecorded ? null : () async {
+                                  Dialogs.showAudioPreviewDialogForRecording(File(audioPath), context);
+                                },
+                                child: const Text("Preview")
+                            ),
+                            TextButton(
+                              onPressed: () {
+                                if(!_isRecording){
+                                  audioPath = '';
+                                  _hasRecorded = false;
+                                  Navigator.pop(context);
+                                }
+                              },
+                              child: const Text("Cancel"),
+                            ),
+                          ],
+                        );
+                      }
+                  );
+                },
+              );
+            },
+          )
+        ],
       ),
       drawer: const AppDrawer(),
       body: Center(
@@ -67,11 +169,8 @@ class _MyHomePageState extends State<MyHomePage> {
                   if (result != null) {
                     PlatformFile file = result.files.first;
                     print('File picked: ${file.name}');
-                    String? prediction = await Api.uploadFile(File(file.path!));
-                    if(prediction != null){
-                      if(!context.mounted) return;
-                      Dialogs.showConfirmationDialog(prediction, context);
-                    }
+                    if(!context.mounted) return;
+                    Dialogs.showAudioPreviewDialog(file, context);
                   } else {
                     print('File picking canceled');
                   }
